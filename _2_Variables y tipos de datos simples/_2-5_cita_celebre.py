5194770e518babbc26c3aa117521c74d6f799bbc
Alan Alexis <106125751+Alan15Alexis@users.noreply.github.com>
2026-03-06 06:41:15 -0600

    genial

--- _2_Variables y tipos de datos simples/_2-5_cita_celebre.py
+++ _2_Variables y tipos de datos simples/_2-5_cita_celebre.py
@@ -1,6 +1,6 @@
 #LAs "f" CADENAS
 #Nos permite concatenar dos variables: 
-#
+#Genial
 autor = "Walt Disney"
 frase = 'Preguntate si lo que estas haciendo hoy te acerca al lugar en el que qeuieres estar mañana.'
 print(f'{autor}, "{frase}"')
