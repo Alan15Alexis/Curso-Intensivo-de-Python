e9e105ce97712b50a41043e30b2be24a14b0b0f7
Alan Alexis <106125751+Alan15Alexis@users.noreply.github.com>
2026-03-06 11:41:23 +0000

    explicacion de concatenacion

--- _2_Variables y tipos de datos simples/_2-5_cita_celebre.py
+++ _2_Variables y tipos de datos simples/_2-5_cita_celebre.py
@@ -1,3 +1,6 @@
+#LAs "f" CADENAS
+#Nos permite concatenar dos variables: 
+#
 autor = "Walt Disney"
 frase = 'Preguntate si lo que estas haciendo hoy te acerca al lugar en el que qeuieres estar mañana.'
 print(f'{autor}, "{frase}"')
